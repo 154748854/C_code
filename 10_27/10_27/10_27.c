c65556cd0b8f2d824ae6ca822c6150b19cdd112a
bo wen <18272262790@163.com>
2024-10-27 19:47:38 +0800

    do while 、continue和break

---
+++ 10_27/10_27/10_27.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//
+//	//do while循环
+//	//在屏幕上打印1-10
+//	/*int i = 1;
+//	do
+//	{
+//
+//		printf("%d ", i);
+//		i++;
+//	}
+//	while (i <= 10);*/
+//
+//
+//	//输入一个正整数来计算这个整数是几位数
+//
+//	unsigned int i = 0;
+//	scanf("%d ", &i);
+//
+//	int count = 0;
+//
+//
+//	do
+//	{
+//		count++;
+//		i = i / 10;
+//	} while (i);
+//
+//	printf("%d\n", count);
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+//break 和 continue
+int main()
+{
+	
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	/*if (i == 5)
+	//		break;*/  //break用于终止整个循环
+
+	//	printf("%d \n", i);
+	//	i++;
+	//	
+	//}
+
+	//int i = 0;
+	//while (i < 10)
+	//{
+
+	//	//if (i == 5)
+	//	//{
+	//	//	continue;
+	//	//}
+	//	//printf("%d \n", i);
+	//	//i++;//i永远到不了6，进入死循环
+
+	//
+	//		
+	//		i++;
+	//		if (i == 5)
+	//		{
+	//			continue;
+	//		}
+	//			printf("%d \n", i);
+
+	//}
+
+	//int i = 10;
+	//for (i = 1; i <= 10; i++)
+	//{
+	//	if (i == 5)
+	//	{
+	//		continue;
+	//		     //break;
+	//	}
+	//	printf("%d ", i);
+	//}
+
+	return 0;
+}
