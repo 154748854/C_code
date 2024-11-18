c517df980966da2424009eb959d0759c3866d7d3
bo wen <18272262790@163.com>
2024-11-18 23:05:08 +0800

    调试

---
+++ 11_18/11_18/11_18.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	//给数组元素赋值
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//
+//	//打印数组内容
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+//
+
+
+void test()
+{
+	printf("hehe\n");
+	int n = 4 + 5;
+	printf("%d\n", n);
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = a + b;
+	test();
+	printf("hehe\n");
+
+	return 0;
+}
