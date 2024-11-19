1bc5b5e945737599094a74103a6c51a147aa05d6
bo wen <18272262790@163.com>
2024-11-19 21:41:31 +0800

    调试

---
+++ 11_19/11_19/11_19.c
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int num = 100;
+//	char c = 'w';
+//
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+
+
+
+//Çó1!+ 2!+ 3!+ 4!+ ...10!µÄºÍ
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	int i = 1;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	int i = 1;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//		
+//	}
+//	printf("%d ", sum);
+//	
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+//void test1(int arr3[])
+//{
+//
+//}
+//
+//void test2(int arr4[3][5])
+//{
+//
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[3][5] = { 1,2,3,4,5, 2,3,4,5,6, 3,4,5,6,7 };
+//	test1(arr1);
+//	test2(arr2);
+//
+//
+//	return 0;
+//}
+
