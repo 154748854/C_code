7867c41c05efe397f246cde72da84b7bfca773ca
bo wen <18272262790@163.com>
2024-11-28 22:49:31 +0800

    const修饰变量

---
+++ 11_28/11_28/11_28.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//
+//	/*const int num = 100;
+//	num = 200;
+//
+//	printf("%d\n", num);*/
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	const int n = 10;
+//	int* p = &n;
+//	*p = 200;
+//	printf("%d\n", n);
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	/*int n = 10;
+	int m = 200;
+	int* p = &n;
+	*p = 20;
+	p = &m;*/
+
+	int n = 10;
+	int m = 200;
+	////int const * p = &n;
+	//////*p = 20;
+	////p = &m;
+
+	//int* const p = &n;
+	//*p = 10;
+	////p = &m;
+	//printf("%d\n", *p);
+
+	return 0;
+}
